00e74ccbdfc6d0c5219024632faa0fb1477fa402
martynakulis <108092908+martynakulis@users.noreply.github.com>
2023-12-16 15:37:12 +0000

    Deploying to gh-pages from @ martynakulis/goit-js-hw-09@a6ddac630efb844cc7b80883aa7b1d945e5d061c 🚀

---
+++ 01-color-switcher.e287f374.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");e.setAttribute("disabled","");let d=null;t.addEventListener("click",(()=>{t.setAttribute("disabled",""),e.removeAttribute("disabled",""),d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(()=>{e.setAttribute("disabled",""),t.removeAttribute("disabled",""),clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.e287f374.js.map
